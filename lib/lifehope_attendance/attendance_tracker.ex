61694ffa7deedf849b224d069d6eeb40f58d3802
Joel Byler <jbyler@covermymeds.com>
2016-12-09 13:45:23 -0500

    cleanup last commit

---
+++ lib/lifehope_attendance/attendance_tracker.ex
@@ -0,0 +1,31 @@
+defmodule LifehopeAttendance.AttendanceTracker do
+
+  import Ecto.Query
+
+  alias LifehopeAttendance.Event
+  alias LifehopeAttendance.Member
+  alias LifehopeAttendance.EventOccurrence
+  alias LifehopeAttendance.Repo
+  alias LifehopeAttendance.Attendance
+
+  def tracking_data_for(event_occurrence_id) do
+    event_occurrence = Repo.get!(EventOccurrence, event_occurrence_id)
+    attendees = Repo.all(from d in Attendance, where: d.event_occurrence_id == ^event_occurrence_id)
+    members = Repo.all(Member) |> Enum.map(fn(member) -> map_member_for_view(member, attendees) end)
+    event_occurrence
+      |> Map.put(:members, members)
+      |> Map.put(:attendees, attendees)
+  end
+
+  defp map_member_for_view(member, attendees) do
+    %{
+      id: member.id,
+      name: "#{member.last_name}, #{member.first_name}",
+      attendee: attendee_for_member(member.id, attendees)
+    }
+  end
+
+  defp attendee_for_member(member_id, attendees) do
+    Enum.find(attendees, fn(attendee) -> attendee.member_id == member_id end)
+  end
+end
