1fd3ab5fdd351ecf783b4a668ee7f293123b80b9
Joel Byler <jbyler@covermymeds.com>
2016-12-09 13:58:07 -0500

    order member list alphabetically

--- lib/lifehope_attendance/attendance_tracker.ex
+++ lib/lifehope_attendance/attendance_tracker.ex
@@ -9,9 +9,11 @@ defmodule LifehopeAttendance.AttendanceTracker do
   alias LifehopeAttendance.Attendance
 
   def tracking_data_for(event_occurrence_id) do
+    query = from Member, order_by: [asc: :last_name, asc: :first_name]
+
     event_occurrence = Repo.get!(EventOccurrence, event_occurrence_id)
     attendees = Repo.all(from d in Attendance, where: d.event_occurrence_id == ^event_occurrence_id)
-    members = Repo.all(Member) |> Enum.map(fn(member) -> map_member_for_view(member, attendees) end)
+    members = Repo.all(query) |> Enum.map(fn(member) -> map_member_for_view(member, attendees) end)
     event_occurrence
       |> Map.put(:members, members)
       |> Map.put(:attendees, attendees)
