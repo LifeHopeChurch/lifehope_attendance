2b43968e0e55fb94d3451f4e2cb493e5f3289634
Joel Byler <jbyler@covermymeds.com>
2017-02-26 23:05:49 -0500

    Add elm to app

--- web/static/js/app.js
+++ web/static/js/app.js
@@ -19,3 +19,5 @@ import "phoenix_html"
 // paths "./socket" or full ones "web/static/js/socket".
 
 // import socket from "./socket"
+const elmContainer = document.querySelector("#main");
+const elmApp = Elm.Main.embed(elmContainer); 
